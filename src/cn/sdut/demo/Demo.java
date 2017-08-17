726cc47ca86349695aae5f7283c300577f21e0e3
lichangxin <test01@test.com>
2017-08-17 08:53:16 +0800

    commit

--- src/cn/sdut/demo/Demo.java
+++ src/cn/sdut/demo/Demo.java
@@ -2,6 +2,6 @@ package cn.sdut.demo;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("this is a demo321.");
+        System.out.println("this is a demo.");
     }
 }
