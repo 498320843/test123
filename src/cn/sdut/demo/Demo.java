947e430f67ceac53771e613dd348de044d631ad7
lichangxin <test01@test.com>
2017-08-17 08:58:17 +0800

    1

--- src/cn/sdut/demo/Demo.java
+++ src/cn/sdut/demo/Demo.java
@@ -2,6 +2,6 @@ package cn.sdut.demo;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("this is a demo.");
+        System.out.println("this is a demo123.");
     }
 }
